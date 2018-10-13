2d6e4b0852c5b1dd9638715dfd8ed91a3d8836f0
zoe <z.zoelec2@gmail.com>
2018-10-12 20:38:28 -0700

    chore: move dataset

--- CoreClassifier/loader.py
+++ CoreClassifier/loader.py
@@ -1,7 +1,7 @@
 import turicreate as tc
 import os
 
-data = tc.image_analysis.load_images('dataset')
+data = tc.image_analysis.load_images('../dataset')
 
 data['possition'] = data['path'].apply(
     lambda path: os.path.basename(os.path.dirname(path)))
